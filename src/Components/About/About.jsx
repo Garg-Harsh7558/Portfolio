6644bc9c6e471c3a343ff22ffd55fe84fc9615fe
Garg-Harsh7558 <harshagrawal7558@gmail.com>
2025-08-13 23:29:37 +0530

    updating text

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -20,7 +20,7 @@ export const About = () => {
             </h1>
             <div className="flex md:text-3xl font-bold text-white my-1 lg:my-2">
               <h2 className="">I am a</h2>
-              <h2 className="text-white ml-2"> Lorem, ipsum.</h2>
+              <h2 className="text-white ml-2"> Developer</h2>
               {/* <h2>
                 <ReactTypingEffect
                   text={["lorem1", "lorem2", "lorem3"]}
@@ -36,13 +36,13 @@ export const About = () => {
             </div>
             {/* about section */}
             <p className="font-semibold lg:text-xl md:text-lg my-10 text-white lg:w-full text-justify">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-              quibusdam nostrum eum! Accusantium, earum eligendi eos ducimus
-              odit sint, reprehenderit veritatis aliquid expedita eius quo
-              adipisci necessitatibus quibusdam voluptatum Lorem, ipsum.
+              I am a passionate full stack web developer with hands-on
+              experience in modern web technologies. I have a strong foundation
+              in React.js, HTML, CSS, and JavaScript, complemented by practical
+              experience working with Next.js and MongoDB.
             </p>
             <a
-              href="https://www.flipkart.com/?affid=affveve&affExtParam1=d51d956ad29db92b85d44cea2abab916&affExtParam2=60827"
+              href="https://drive.google.com/file/d/1TfmXatHyj3MS8r9yOKsvTvGzutxbDKhB/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               className="bg-[#8245ec] rounded-full text-xl font-semibold p-1.5  hover:text-white transition justify-center flex my-9 duration-[1500ms] hover:font-extrabold hover:shadow-[0_0_40px_#864ee7] "
@@ -50,8 +50,13 @@ export const About = () => {
               Download CV
             </a>
           </div>
-          <div className="md:w-1/2 "><Tilt className="sm:ml-8 md:ml-24 md:mt-24">
-            <img src={harsh} alt="Harsh Agrawal" className="rounded-full lg:ml-10 drop-shadow-[0_10px_20px_rgba(130,69,255,0.5)]"/>
+          <div className="md:w-1/2 ">
+            <Tilt className="sm:ml-8 md:ml-24 md:mt-24">
+              <img
+                src={harsh}
+                alt="Harsh Agrawal"
+                className="rounded-full lg:ml-10 drop-shadow-[0_10px_20px_rgba(130,69,255,0.5)]"
+              />
             </Tilt>
           </div>
         </div>
